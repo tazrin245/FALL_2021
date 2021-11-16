d0453a260ec02f2994214df12c23ff4959c90484
tazrin245 <75943074+tazrin245@users.noreply.github.com>
2021-11-16 23:37:11 +0600

    Add files via upload

---
+++ FTTask1/from/from/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="asset/css/style.css">
+</head>
+<body>
+    <div class="container">
+    <h1>ABC Managment System</h1>
+    <h2>We create Future</h2>
+    </div>
+   
+    <div class="menu-bar">
+<ul>
+  <li>Home</li>
+  <li>About Us</li>
+  <li>Shop</li>
+
+</ul>
+
+    </div>
+    <div class="registion">
+        <h2>Registration From</h2>
+    </div>
+    <div class="from">
+
+     <table>
+         <tr>
+             <td>First Name</td>
+             <td><input type="text"placeholder="First Name" name=""></td>
+         </tr>
+         <tr>
+             <td>Last Name</td>
+             <td><input type="text"placeholder="Last Name" name=""></td>
+         </tr>
+         <tr>
+             <td>Age</td>
+             <td><input type="text"placeholder="Age" name=""></td>
+         </tr>
+         <tr>
+             <td>Designation</td>
+             <td><input type="radio" name="">Junior programer
+             <input type="radio" name="">Senior Programmer
+             <input type="radio" name="">Project Manager
+
+            </td>
+         </tr>
+         <tr>
+             <td>Preferred Language</td>
+             <td><input type="radio" name="">Java
+                 <input type="radio" name="">PHP
+                 <input type="radio" name="">C++
+
+            </td>
+         </tr>
+         <tr>
+             <td>Email</td>
+             <td><input type="text"placeholder="Email" name=""></td>
+         </tr>
+         <tr>
+             <td>Password</td>
+             <td><input type="text"placeholder="Password" name=""></td>
+         </tr>
+         <tr>
+             <td>Pleace choose a file</td>
+             <td><input type="file"placeholder="Choose File" name=""></td>
+         </tr>
+     </table>
+    </div>
+    <div class="submit">
+    <input type="Submit">
+    <input type="Reset">
+    </div>
+    
+
+    
+</body>
+</html>
